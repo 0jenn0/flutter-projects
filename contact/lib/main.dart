f4d275015b5013b729053894d194457a67f2ce35
0jenn0 <jenn0.6n@gmail.com>
2023-06-27 05:30:45 +0900

    scaffold

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -9,19 +9,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Row(
-          mainAxisAlignment : MainAxisAlignment.spaceEvenly
-         children: [
-          Icon(Icons.star),
-          Icon(Icons.star)
-         ],
-        ),
-        bottomNavigationBar: BottomAppBar(child: Text('메뉴'))
-      )
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('앱임'),
+              centerTitle: false,
+            ),
+            body: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [Text('안녕')],
+            ),
+            bottomNavigationBar: BottomAppBar(
+                child: SizedBox(
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.phone),
+                  Icon(Icons.message),
+                  Icon(Icons.contact_page)
+                ],
+              ),
+            ))));
   }
 }
