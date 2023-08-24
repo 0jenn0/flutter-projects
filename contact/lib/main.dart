c2a2bc74c91fedf648289dcc54c805232ac62ad4
0jenn0 <jenn0.6n@gmail.com>
2023-08-24 17:39:07 +0900

    custom widget

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -50,66 +50,7 @@ class MyApp extends StatelessWidget {
                 size: 25,
               ),
             ),
-            body: Container(
-              width: double.infinity,
-              height: 140,
-              margin: EdgeInsets.all(15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Flexible(
-                    flex: 5,
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                      child: Image.asset('assets/img.jpg', fit: BoxFit.fill),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 5,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        RichText(
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          text: TextSpan(
-                              text: '캐논 DSLR 100D (단렌즈,충전기 16기가SD 포함,흥정 불가능)',
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.black)),
-                        ),
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            '성동구 행당동 끌올 10분 전',
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            '210,000원',
-                            style: TextStyle(fontWeight: FontWeight.w800),
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.favorite_border),
-                              iconSize: 20,
-                            ),
-                            Text('3')
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
+            body: ShopItem(),
             bottomNavigationBar: BottomAppBar(
                 child: SizedBox(
               height: 50,
@@ -122,5 +63,72 @@ class MyApp extends StatelessWidget {
                 ],
               ),
             ))));
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 140,
+      margin: EdgeInsets.all(15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            flex: 5,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: Image.asset('assets/img.jpg', fit: BoxFit.fill),
+            ),
+          ),
+          Flexible(
+            flex: 5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RichText(
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  text: TextSpan(
+                      text: '캐논 DSLR 100D (단렌즈,충전기 16기가SD 포함,흥정 불가능)',
+                      style: TextStyle(fontSize: 18, color: Colors.black)),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    '성동구 행당동 끌올 10분 전',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    '210,000원',
+                    style: TextStyle(fontWeight: FontWeight.w800),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite_border),
+                      iconSize: 20,
+                    ),
+                    Text('3')
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
