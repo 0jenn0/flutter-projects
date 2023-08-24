1d2e7f4a37b833a9fb699abb565db99b0c00eb20
최재희 <choejaehui@choejaehuiui-MacBookAir.local>
2023-08-24 16:39:11 +0900

    flexible

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -12,13 +12,103 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: Text('앱임'),
+              title: Row(
+                children: [
+                  Text('금호동3가'),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    color: Colors.black,
+                  )
+                ],
+              ),
               centerTitle: false,
+              backgroundColor: Colors.white,
+              titleTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18),
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.search,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.menu,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.notifications_none,
+                    )),
+              ],
+              actionsIconTheme: IconThemeData(
+                color: Colors.black,
+                size: 25,
+              ),
             ),
-            body: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Text('안녕')],
+            body: Container(
+              width: double.infinity,
+              height: 140,
+              margin: EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    flex: 5,
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                      child: Image.asset('assets/img.jpg', fit: BoxFit.fill),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 5,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RichText(
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                          text: TextSpan(
+                              text: '캐논 DSLR 100D (단렌즈,충전기 16기가SD 포함,흥정 불가능)',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.black)),
+                        ),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            '성동구 행당동 끌올 10분 전',
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            '210,000원',
+                            style: TextStyle(fontWeight: FontWeight.w800),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.favorite_border),
+                              iconSize: 20,
+                            ),
+                            Text('3')
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
             bottomNavigationBar: BottomAppBar(
                 child: SizedBox(
