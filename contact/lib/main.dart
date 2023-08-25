ff40a00f75e9c46b21eb16c0dc8cad304905a641
0jenn0 <jenn0.6n@gmail.com>
2023-08-25 13:59:09 +0900

    context dialog

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -12,67 +12,116 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
-  var name = ['하니', '다니엘', '해린'];
+  List<Map<String, dynamic>> data = [
+    {'name': '하니', 'likes': 0},
+    {'name': '다니엘', 'likes': 0},
+    {'name': '해린', 'likes': 0}
+  ];
+  void _incrementLike(int index) {
+    setState(() {
+      data[index]['likes'] += 1;
+      // data = deepCopyData(data);
+    });
+  }
+
+  List<Map<String, dynamic>> deepCopyData(
+      List<Map<String, dynamic>> originalData) {
+    return originalData.map((item) {
+      return Map<String, dynamic>.from(item);
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    a++;
-                  });
-                },
-                child: Text(a.toString())),
-            appBar: AppBar(
-              title: Row(
-                children: [
-                  Text('연락처앱'),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.keyboard_arrow_down),
-                    color: Colors.black,
-                  )
-                ],
-              ),
-              centerTitle: false,
-              backgroundColor: Colors.white,
-              titleTextStyle: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18),
-              actions: [
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.search,
-                    )),
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.menu,
-                    )),
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.notifications_none,
-                    )),
-              ],
-              actionsIconTheme: IconThemeData(
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return Dialog(
+                    child: Container(
+                  padding: EdgeInsets.all(20),
+                  height: 200,
+                  width: 300,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Contact',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      TextField(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('Cancel')),
+                          TextButton(onPressed: () {}, child: Text('Ok'))
+                        ],
+                      )
+                    ],
+                  ),
+                ));
+              });
+        }),
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Text('연락처앱'),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.keyboard_arrow_down),
                 color: Colors.black,
-                size: 25,
-              ),
-            ),
-            body: ListView.builder(
-                itemCount: 3,
-                itemBuilder: (c, i) {
-                  return ListTile(
-                      leading: Image.asset('assets/img.jpg'),
-                      title: Text(name[i]));
-                }),
-            bottomNavigationBar: BottomBar()));
+              )
+            ],
+          ),
+          centerTitle: false,
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.menu,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications_none,
+                )),
+          ],
+          actionsIconTheme: IconThemeData(
+            color: Colors.black,
+            size: 25,
+          ),
+        ),
+        body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (c, i) {
+              return ListTile(
+                leading: Text(data[i]['likes'].toString()),
+                title: Text(data[i]['name'].toString()),
+                trailing: TextButton(
+                  child: Text('좋아요'),
+                  onPressed: () {
+                    _incrementLike(i);
+                  },
+                ),
+              );
+            }),
+        bottomNavigationBar: BottomBar());
   }
 }
 
