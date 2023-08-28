6c37fae47848c9e2df266b64241c2b5423977ab9
0jenn0 <jenn0.6n@gmail.com>
2023-08-28 19:48:43 +0900

    infinity scrollview

--- insta/lib/main.dart
+++ insta/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './style.dart' as style;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/rendering.dart'; // 스크롤 다룰 때 임포트해놓고 시작하면 좋음
 
 void main() {
   runApp(MaterialApp(home: MyApp(), theme: style.theme));
@@ -29,6 +30,14 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  addData(String url) async {
+    var result =
+        await http.get(Uri.parse(url)).then((value) => jsonDecode(value.body));
+    setState(() {
+      data.add(result);
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -49,7 +58,7 @@ class _MyAppState extends State<MyApp> {
           IconButton(onPressed: () {}, icon: Icon(Icons.add_box_outlined))
         ],
       ),
-      body: [Home(data: data), Text('Shop')][tab],
+      body: [Home(data: data, addData: addData), Text('Shop')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -72,26 +81,48 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key, this.data});
+class Home extends StatefulWidget {
+  const Home({super.key, this.data, this.addData});
   final data;
+  final addData;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var scroll = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() async {
+      if (scroll.position.pixels == scroll.position.maxScrollExtent) {
+        widget.addData('https://codingapple1.github.io/app/more1.json');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    print(data);
-    return ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (c, i) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.network(data[i]['image']),
-              Text('likes ' + data[i]['likes'].toString()),
-              Text(data[i]['user']),
-              Text(data[i]['content'])
-              // Text(data[0]['content'])
-            ],
-          );
-        });
+    if (widget.data.isNotEmpty) {
+      return ListView.builder(
+          controller: scroll,
+          itemCount: widget.data.length,
+          itemBuilder: (c, i) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(widget.data[i]['image']),
+                Text('likes ${widget.data[i]['likes']}'),
+                Text(widget.data[i]['user']),
+                Text(widget.data[i]['content'])
+                // Text(data[0]['content'])
+              ],
+            );
+          });
+    } else {
+      return Text('Loading ...');
+    }
   }
 }
